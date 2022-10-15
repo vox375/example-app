a338a6d6f0c237185362d3c2e2907d81b21e122a
k-nakayama <k-nakayama@UniTrust0081.local>
2022-10-15 16:45:36 +0900

    GitHub class tweet となっていたのを class Tweet に 修正

--- app/Models/tweet.php
+++ app/Models/tweet.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tweet extends Model
+class Tweet extends Model
 {
     use HasFactory;
 

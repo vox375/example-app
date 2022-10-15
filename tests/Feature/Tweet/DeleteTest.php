d7748812f46a4b2c013cc2f3856a17c2757f3304
k-nakayama <k-nakayama@UniTrust0081.local>
2022-10-15 16:38:59 +0900

    GitHub DeleteTest.php 修正

--- tests/Feature/Tweet/DeleteTest.php
+++ tests/Feature/Tweet/DeleteTest.php
@@ -12,23 +12,11 @@ class DeleteTest extends TestCase
 {
     use RefreshDatabase;
 
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
     public function test_delete_successed()
     {
-        // $user = User::factory()->create(); // ユーザーを作成
+        $user = User::factory()->create(); // ユーザーを作成
 
-        // $tweet = Tweet::factory()->create(['user_id' => $user->id]); // つぶやきを作成
+        $tweet = Tweet::factory()->create(['user_id' => $user->id]); // つぶやきを作成
 
         // $this->actingAs($user); // 指定したユーザーでログインした状態にする
 
